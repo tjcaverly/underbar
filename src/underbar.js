2a8aeb2a405cd7daccd4723b12aa477e9ed01ca1
Trevor Caverly <trevorcaverly@gmail.com>
2015-02-15 11:34:35 -0500

    _.every passes tests

--- src/underbar.js
+++ src/underbar.js
@@ -221,6 +221,15 @@
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
 
+    iterator = iterator || _.identity;
+
+    return _.reduce(collection, function(isTrue, item) {
+      if (!(isTrue)){
+        return false;
+      }
+      return Boolean(iterator(item));
+    }, true);
+
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
